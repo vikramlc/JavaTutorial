f788de4763ed253f84f4ce393c5cf8dc20d2f82c
vikramlc <vikram.chawan121@gmail.com>
2019-06-29 16:27:30 +0530

    Synchronization part 2 - synchronising a block

--- src/com/vikramlc/Main.java
+++ src/com/vikramlc/Main.java
@@ -18,10 +18,15 @@ class Countdown {
 
     private int i;
 
-    //Synchronized: Every object has an intrinsic lock. Each thread has to obtain that lock and then it can run the operation.
-    // So in synchronization we synchronize a block or a function and that means the thread has to obtain a lock and then it can
-    // proceed to execute the function or block. Please note each time only thread can execute the function or block.
-    public synchronized void doCountdown() {
+    // when a method is synchronized only one thread can execute that at a time so when the thread is executing the
+    // method all other threads that want to call the method or any other synchronized method in that class will suspend
+    // until the thread running the method exits it then another thread can run a
+    // synchronized method then another etc to be clear if a class has three
+    // synchronize methods then only one of these methods can ever run at a time and
+    // only on one thread now since only one thread can execute a synchronized method
+    // at a time
+    // public synchronized void doCountdown() {
+    public void doCountdown() {
         String color;
 
         switch(Thread.currentThread().getName()) {
@@ -35,8 +40,16 @@ class Countdown {
                 color = ThreadColor.ANSI_GREEN;
         }
 
-        for(i=10; i>0; i--) {
-            System.out.println(color + Thread.currentThread().getName() + ": i= " + i);
+        //Synchronized: Every object has an intrinsic lock. Each thread has to obtain that lock and then it can run the operation.
+        // So in synchronization we synchronize a block or a function and that means the thread has to obtain a lock and then it can
+        // proceed to execute the function or block. Please note each time only thread can execute the function or block.
+        // NOTE: Use synchronize always on a object instance or global variable and not on a local variable as it will be stored in Thread Stack.
+        // Thread will keep a copy of the variable in the Thread Stack and the values are stored in the Heap so we see the values
+        // updated by both the Threads.
+        synchronized (this) {
+            for(i=10; i>0; i--) {
+                System.out.println(color + Thread.currentThread().getName() + ": i= " + i);
+            }
         }
     }
 }
