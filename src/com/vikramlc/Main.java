5e44ad9603e6b4629ad13af6d5af2ffe6af00f14
vikramlc <vikram.chawan121@gmail.com>
2019-06-29 12:36:05 +0530

    Multiple Threads

--- src/com/vikramlc/Main.java
+++ src/com/vikramlc/Main.java
@@ -1,44 +1,52 @@
 package com.vikramlc;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println(ThreadColor.ANSI_PURPLE + "Hello from the main thread!");
+        Countdown countdown = new Countdown();
 
-        Thread anotherThread = new AnotherThread();
-        anotherThread.setName("====Another Thread====");
-        anotherThread.start();
+        CountdownThread t1 = new CountdownThread(countdown);
+        t1.setName("Thread 1");
+        CountdownThread t2 = new CountdownThread(countdown);
+        t2.setName("Thread 2");
 
-        new Thread() {
-            @Override
-            public void run() {
-                System.out.println(ThreadColor.ANSI_GREEN + "Hello from the anonymous");
-            }
-        }.start();
+        t1.start();
+        t2.start();
+    }
+}
 
-//        //Usual way without anonymouse class
-//        Thread myRunnableThread = new Thread(new MyRunnable());
-//        myRunnableThread.start();
+class Countdown {
 
-        Thread myRunnableThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println(ThreadColor.ANSI_RED + "Hello from the anonymous runnable class");
+    private int i;
 
-                try {
-                    anotherThread.join(2000);
-                    System.out.println(ThreadColor.ANSI_RED + "Another thread executed, now I am awake.");
-                } catch(InterruptedException e) {
-                    System.out.println(ThreadColor.ANSI_RED + "Another thread was interrupted and not executed completely");
-                }
-            }
-        });
+    public void doCountdown() {
+        String color;
 
-        myRunnableThread.start();
+        switch(Thread.currentThread().getName()) {
+            case "Thread 1":
+                color = ThreadColor.ANSI_CYAN;
+                break;
+            case "Thread 2":
+                color = ThreadColor.ANSI_PURPLE;
+                break;
+            default:
+                color = ThreadColor.ANSI_GREEN;
+        }
 
-        //anotherThread.interrupt(); // This is called when anotherThread is sleeping so an interrupted exception is thrown.
+        for(i=10; i>0; i--) {
+            System.out.println(color + Thread.currentThread().getName() + ": i= " + i);
+        }
+    }
+}
 
-        System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main thread.");
-        //anotherThread.start(); // Gives illegal state exception
+class CountdownThread extends Thread {
+    private Countdown countdownThread;
+
+    public CountdownThread(Countdown countdown) {
+        this.countdownThread = countdown;
+    }
+
+    @Override
+    public void run() {
+        countdownThread.doCountdown();
     }
 }
