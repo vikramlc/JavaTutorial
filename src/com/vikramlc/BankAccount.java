aaf37a0e9f4631aa201f69427bed5ed6214c161f
vikramlc <vikram.chawan121@gmail.com>
2019-07-04 22:00:01 +0530

    using locks instead of synchrinize keyword

--- src/com/vikramlc/BankAccount.java
+++ src/com/vikramlc/BankAccount.java
@@ -1,23 +1,46 @@
 package com.vikramlc;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
     private double balance;
     private String accountNumber;
+    private ReentrantLock lock;
 
-    public BankAccount(String accountNumber, double initialBalance) {
+    public BankAccount(String accountNumber, double initialBalance, ReentrantLock lock) {
         this.accountNumber = accountNumber;
         this.balance = initialBalance;
+        this.lock = lock;
     }
 
-    public synchronized void deposit(double amount) {
-        balance += amount;
+    public void deposit(double amount) {
+        lock.lock();
+        try {
+            balance += amount;
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void withdraw(double amount) {
-        balance -= amount;
+    public void withdraw(double amount) {
+        lock.lock();
+        try {
+            balance -= amount;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public double getBalance() {
         return balance;
     }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void printAccountNumber() {
+        System.out.println("Account Number: " + accountNumber);
+    }
+
 }
--- src/com/vikramlc/Main.java
+++ src/com/vikramlc/Main.java
@@ -1,15 +1,20 @@
 package com.vikramlc;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
+    private static ReentrantLock lock = new ReentrantLock();
+
     public static void main(String[] args) {
-        BankAccount account = new BankAccount("12345-678", 1000.00);
+        BankAccount account = new BankAccount("12345-678", 1000.00, lock);
 
         new Thread(new Runnable() {
             @Override
             public void run() {
                 account.deposit(300.00);
                 account.withdraw(50.00);
+                account.printAccountNumber();
                 System.out.println("Thread 1: Account balance = " + account.getBalance());
             }
         }).start();
@@ -19,6 +24,7 @@ public class Main {
             public void run() {
                 account.deposit(203.75);
                 account.withdraw(100.00);
+                account.printAccountNumber();
                 System.out.println("Thread 2: Account balance = " + account.getBalance());
             }
         }).start();
