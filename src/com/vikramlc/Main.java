6913cc5dab2f4d543f881e80f682f25c7c08345b
vikramlc <vikram.chawan121@gmail.com>
2019-07-04 16:55:15 +0530

    Deadlock scenario 2 - synchronized

--- src/com/vikramlc/Main.java
+++ src/com/vikramlc/Main.java
@@ -1,10 +1,15 @@
 package com.vikramlc;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
     public static void main(String[] args) {
-        final PolitePerson ajay = new PolitePerson("deepa");
-        final PolitePerson deepa = new PolitePerson("ajay");
+
+        ReentrantLock lock = new ReentrantLock();
+
+        final PolitePerson ajay = new PolitePerson("deepa", lock);
+        final PolitePerson deepa = new PolitePerson("ajay", lock);
 
         new Thread(new Runnable() {
             @Override
@@ -23,18 +28,23 @@ public class Main {
 
     public static class PolitePerson {
         private String name;
+        ReentrantLock lock;
 
-        public PolitePerson(String name) {
+        public PolitePerson(String name, ReentrantLock lock) {
             this.name = name;
+            this.lock = lock;
         }
 
         public String getName() {
             return name;
         }
 
-        public synchronized void sayHello(PolitePerson person) {
+        public void sayHello(PolitePerson person) {
+
+            lock.lock();
             System.out.format("%s: %s" + " has said Hello to me.%n", this.name, person.getName());
             person.sayHelloBack(this);
+            lock.unlock();
         }
 
         public synchronized void sayHelloBack(PolitePerson person) {
