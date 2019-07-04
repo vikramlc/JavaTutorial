e2e30d8e7876f7d966f934ec34f797ddc1aa177d
vikramlc <vikram.chawan121@gmail.com>
2019-07-05 00:19:37 +0530

    spot and fix - had been over-synchronized

--- src/com/vikramlc/Main.java
+++ src/com/vikramlc/Main.java
@@ -1,99 +1,72 @@
 package com.vikramlc;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Main {
 
     public static void main(String[] args) {
-        BankAccount account1 = new BankAccount("12345-678", 500.00);
-        BankAccount account2 = new BankAccount("98765-432", 1000.00);
+        Tutor tutor = new Tutor();
+        Student student = new Student(tutor);
+        tutor.setStudent(student);
 
-        new Thread(new Transfer(account1, account2, 10.00), "Transfer1").start();
-        new Thread(new Transfer(account2, account1, 55.88), "Transfer2").start();
+        Thread tutorThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                tutor.studyTime();
+            }
+        });
+
+        Thread studentThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                student.handInAssignment();
+            }
+        });
+
+        tutorThread.start();
+        studentThread.start();
     }
 }
 
-class BankAccount {
-    private double balance;
-    private String accountNumber;
-    private Lock lock = new ReentrantLock();
+class Tutor {
+    private Student student;
 
-    BankAccount(String accountNumber, double balance) {
-        this.accountNumber = accountNumber;
-        this.balance = balance;
+    public void setStudent(Student student) {
+        this.student = student;
     }
 
-    public boolean withdraw(double amount) {
-        if (lock.tryLock()) {
-
-            try {
-                balance -= amount;
-
-                try {
-                    // Simulate database access
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                }
-            } finally {
-                lock.unlock();
-            }
-
-            System.out.printf("%s: Withdrew %f\n", Thread.currentThread().getName(), amount);
-            return true;
+    public synchronized void studyTime() {
+        System.out.println("Tutor has arrived");
+        try {
+            // wait for student to arrive and hand in assignment
+            Thread.sleep(300);
         }
-        return false;
+        catch (InterruptedException e) {
+
+        }
+        student.startStudy();
+        System.out.println("Tutor is studying with student");
     }
 
-    public boolean deposit(double amount) {
-        if (lock.tryLock()) {
-            try {
-                balance += amount;
-                try {
-                    // Simulate database access
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                }
-            } finally {
-                lock.unlock();
-            }
-
-            System.out.printf("%s: Deposited %f\n", Thread.currentThread().getName(), amount);
-            return true;
-        }
-        return false;
-    }
-
-    public boolean transfer(BankAccount destinationAccount, double amount) {
-        if (withdraw(amount)) {
-            if (destinationAccount.deposit(amount)) {
-                return true;
-            }
-            else {
-                // The deposit failed. Refund the money back into the account.
-                System.out.printf("%s: Destination account busy. Refunding money\n",
-                        Thread.currentThread().getName());
-                deposit(amount);
-            }
-        }
-
-        return false;
+    public void getProgressReport() {
+        // get progress report
+        System.out.println("Tutor gave progress report");
     }
 }
 
-class Transfer implements Runnable {
-    private BankAccount sourceAccount, destinationAccount;
-    private double amount;
+class Student {
 
-    Transfer(BankAccount sourceAccount, BankAccount destinationAccount, double amount) {
-        this.sourceAccount = sourceAccount;
-        this.destinationAccount = destinationAccount;
-        this.amount = amount;
+    private Tutor tutor;
+
+    Student(Tutor tutor) {
+        this.tutor = tutor;
     }
 
-    public void run() {
-        while (!sourceAccount.transfer(destinationAccount, amount))
-            continue;
-        System.out.printf("%s completed\n", Thread.currentThread().getName());
+    public void startStudy() {
+        // study
+        System.out.println("Student is studying");
+    }
+
+    public synchronized void handInAssignment() {
+        tutor.getProgressReport();
+        System.out.println("Student handed in assignment");
     }
 }
