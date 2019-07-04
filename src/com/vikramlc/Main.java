157a8277dc840e85570d2f47435b1b2a07e43328
vikramlc <vikram.chawan121@gmail.com>
2019-07-04 18:06:07 +0530

    thread starvation - with fairness solution

--- src/com/vikramlc/Main.java
+++ src/com/vikramlc/Main.java
@@ -1,8 +1,10 @@
 package com.vikramlc;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
 
-    private static Object lock = new Object();
+    private static ReentrantLock lock = new ReentrantLock(true);
 
     public static void main(String[] args) {
         Thread t1 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 10");
@@ -11,11 +13,11 @@ public class Main {
         Thread t4 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 4");
         Thread t5 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 2");
 
-//        t1.setPriority(10);
-//        t2.setPriority(8);
-//        t3.setPriority(6);
-//        t4.setPriority(4);
-//        t5.setPriority(2);
+        t1.setPriority(10);
+        t2.setPriority(8);
+        t3.setPriority(6);
+        t4.setPriority(4);
+        t5.setPriority(2);
 
         t3.start();
         t5.start();
@@ -34,9 +36,12 @@ public class Main {
 
         @Override
         public void run() {
-            synchronized (lock) {
-                for(int i=0; i<100; i++) {
+            for (int i = 0; i < 100; i++) {
+                lock.lock();
+                try {
                     System.out.format(color + "%s: %d\n", Thread.currentThread().getName(), runCount++);
+                } finally {
+                    lock.unlock();
                 }
             }
         }
