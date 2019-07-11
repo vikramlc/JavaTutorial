b8cdc09fc79251d5c878ac9641f0f6d990c5d5a3
vikramlc <vikram.chawan121@gmail.com>
2019-07-11 11:04:01 +0530

    Lazy initialization

--- src/com/vikramlc/Main.java
+++ src/com/vikramlc/Main.java
@@ -3,16 +3,10 @@ package com.vikramlc;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Result: " + toMilesPerHour(10.5));
+
     }
 
-    public static long toMilesPerHour(double kiloMetersPerHour) {
-        if(kiloMetersPerHour < 0) {
-            return -1;
-        }
 
-        return Math.round(kiloMetersPerHour/1.609);
-    }
 
 
 }
