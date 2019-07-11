d1add32df295aa407e5b0b9a464b517518d0e2b6
vikramlc <vikram.chawan121@gmail.com>
2019-07-11 11:43:32 +0530

    Final correct implementation with serial UID and readResolve

--- src/com/vikramlc/DemoSingleton.java
+++ src/com/vikramlc/DemoSingleton.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class DemoSingleton implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private volatile static DemoSingleton instance = null;
 
     private DemoSingleton() {
