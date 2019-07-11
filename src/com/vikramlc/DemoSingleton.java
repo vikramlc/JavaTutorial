6fc3573044ba6aff403ae9b88ea06176b5f85ed8
vikramlc <vikram.chawan121@gmail.com>
2019-07-11 11:41:23 +0530

    serilaization with readResolve

--- src/com/vikramlc/DemoSingleton.java
+++ src/com/vikramlc/DemoSingleton.java
@@ -17,6 +17,14 @@ public class DemoSingleton implements Serializable {
         return instance;
     }
 
+    // There were two instances of the object created, each when serialized and deserialized.
+    // To avoid this, we need to include a readResolve() method in our DemoSingleton class.
+    // This method will be invoked when you will de-serialize the object.
+    // Inside of this method, you must return the existing instance to ensure a single instance application wide.
+    protected Object readResolve() {
+        return instance;
+    }
+
     private int i = 10;
 
     public int getI() {
